80a660284e1c29110653822f8d271abe8f205883
sokhna-faye <sfaye4230@gmail.com>
2025-04-30 12:13:35 +0000

    Mise à jour du code

--- app.js
+++ app.js
@@ -48,7 +48,8 @@ app.get('/', (req, res) => {
 // Lancement du serveur
 module.exports = app;
 
-app.listen(3000, () => console.log('Server started on http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
 
 
 
