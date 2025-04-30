193f5e716c74a981a2b54fafd74daa50248d53bf
sokhna-faye <sfaye4230@gmail.com>
2025-04-30 11:24:33 +0000

    Mise à jour du code

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 const express = require('express');
-require('dotenv').config();
 const mongoose = require('mongoose');
 const session = require('express-session');
 const serverless = require('serverless-http');
+const path = require('path');
+require('dotenv').config();
+
 const app = express();
 
-// Connexion MongoDB
-// Connexion MongoDB
+// MongoDB connection (utilise variable d'env au lieu de hardcode !)
 mongoose.connect("mongodb+srv://blindecoly:coly826@aplicationxy.kaumhiu.mongodb.net/projetstage?retryWrites=true&w=majority&appName=aplicationxy")
   .then(() => console.log("✅ MongoDB connecté"))
   .catch(err => console.error("Erreur mongoose :", err));
 
-// Configuration EJS
+// EJS
+app.set('views', path.join(__dirname, '../views')); // important pour que EJS fonctionne depuis /api/
 app.set('view engine', 'ejs');
+
 app.use(express.urlencoded({ extended: true }));
+app.use('/uploads', express.static(path.join(__dirname, '../public/uploads')));
 
-// Middleware statique
-app.use('/uploads', express.static('public/uploads'));
-
-// Session
 app.use(session({
   secret: 'secret',
   resave: false,
@@ -31,8 +31,9 @@ app.use((req, res, next) => {
 });
 
 // Routes
-app.use('/', require('./routes/auth'));
-app.use('/hotels', require('./routes/hotel'));
+app.use('/', require('../routes/auth'));
+app.use('/hotels', require('../routes/hotel'));
 
-// Export pour serverless
+// Export Vercel handler
+module.exports = app;
 module.exports.handler = serverless(app);
